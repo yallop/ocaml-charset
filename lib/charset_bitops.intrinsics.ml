463498171fac898003f37639f4972c5ad56200c0
Jeremy Yallop <yallop@gmail.com>
2025-05-05 15:50:13 +0100

    Remove check for intrinsics signatures.
    Recent versions of ocaml_intrinsics export count_leading_zeros as a value, not a primitive; see:
    
    https://github.com/ocaml/opam-repository/pull/27839#issuecomment-2847264144

--- lib/charset_bitops.intrinsics.ml
+++ lib/charset_bitops.intrinsics.ml
@@ -16,16 +16,4 @@ external ctz : int -> int = "caml_int_ctz"
 
 (* Check that the signatures are as expected, since we're re-exporting externals *)
 [@@@ocaml.warning "-32"]
-module Int_ :
-sig
-  external count_leading_zeros : int -> (int [@untagged]) = "caml_int_clz"
-    "caml_int_clz_tagged_to_untagged" [@@noalloc]
-  external count_leading_zeros2 : int -> int = "caml_int_clz"
-    "caml_int_clz_untagged_to_untagged" [@@untagged] [@@noalloc]
-  external count_set_bits : int -> (int [@untagged]) = "caml_int_popcnt"
-    "caml_int_popcnt_tagged_to_untagged" [@@noalloc]
-  external count_set_bits2 : int -> int = "caml_int_popcnt"
-    "caml_int_popcnt_untagged_to_untagged" [@@untagged] [@@noalloc]
-  external count_trailing_zeros : int -> int = "caml_int_ctz"
-    "caml_int_ctz_untagged_to_untagged" [@@untagged] [@@noalloc]
-end = Ocaml_intrinsics.Int
+module Int_ = Ocaml_intrinsics.Int
